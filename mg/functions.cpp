ec2e77727fdfbeed1548649452ec26306b973f09
Mohit Gayatri <mohitgayatri96@gmail.com>
2022-04-21 13:23:14 -0400

    add more test functions

---
+++ mg/functions.cpp
@@ -0,0 +1,35 @@
+#include <math.h>
+
+// test objective funtion
+double test_objective(double * w) {
+    double offsets[3] = {-8., 17., 22.};
+    double objective = 0; 
+    for (int i = 0; i < 3; i++)
+        objective += pow((w[i] - offsets[i]), 2.); 
+    
+    return objective;
+}
+
+// goldstein-price objective function
+double goldstein_price_function(double * w) {
+    double x = w[0], y = w[1]; 
+    double b1 = pow((x + y + 1), 2); 
+    double b2 = 19 - 14*x + 3*pow(x, 2) - 14*y + 6*x*y + 3*pow(y, 2); 
+    double t1 = 1 + b1*b2;
+
+    double b3 = pow(2*x - 3*y, 2); 
+    double b4 = 18 - 32*x + 12*pow(x, 2) + 48*y - 36*x*y + 27*pow(y, 2); 
+    double t2 = 30 + b3*b4;
+
+    return (t1*t2);
+}
+
+double easom_function(double * w) {
+    double x = w[0], y = w[1]; 
+    double t1 = -1 * cos(x) * cos(y); 
+
+    double b1 = pow(x - M_PI, 2) + pow(y - M_PI, 2); 
+    double t2 = exp(-1 * b1); 
+
+    return (t1*t2); 
+}
--- mg/main.cpp
+++ mg/main.cpp
@@ -1,28 +1,5 @@
 #include "genetic_algorithm.cpp"
-
-// test objective funtion
-double test_objective(double * w) {
-    double offsets[3] = {-8., 17., 22.};
-    double objective = 0; 
-    for (int i = 0; i < 3; i++)
-        objective += pow((w[i] - offsets[i]), 2.); 
-    
-    return objective;
-}
-
-// goldstein-price objective function
-double goldstein_price_function(double * w) {
-    double x = w[0], y = w[1]; 
-    double b1 = pow((x + y + 1), 2); 
-    double b2 = 19 - 14*x + 3*pow(x, 2) - 14*y + 6*x*y + 3*pow(y, 2); 
-    double t1 = 1 + b1*b2;
-
-    double b3 = pow(2*x - 3*y, 2); 
-    double b4 = 18 - 32*x + 12*pow(x, 2) + 48*y - 36*x*y + 27*pow(y, 2); 
-    double t2 = 30 + b3*b4;
-
-    return (t1*t2);
-}
+#include "functions.cpp"
 
 int main() {
     // initialize bounds for GA
@@ -33,7 +10,7 @@ int main() {
     double r_cross, r_mut; 
     num_variables = 2; 
     pop_size = 100; 
-    num_gens = 1000; 
+    num_gens = 1000;
     r_cross = 0.8;
     r_mut = 0.1; 
 
@@ -42,14 +19,14 @@ int main() {
         bounds[i] = (double*) malloc(num_variables*sizeof(double));
         for (int j = 0; j < num_variables; j++) {
             if (i == 0)
-                bounds[i][j] = -2.; 
+                bounds[i][j] = -100.; 
             else 
-                bounds[i][j] = 2.;
+                bounds[i][j] = 100.;
         } 
     }
 
     // call GA with given hyperparameters
-    genetic_algorithm(goldstein_price_function, bounds, num_variables, 
+    genetic_algorithm(easom_function, bounds, num_variables, 
         pop_size, num_gens, r_cross, r_mut);     
     
     return 0; 
